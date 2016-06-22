30f86be5ceaeede3e118d4a5195cc5bd124b758b
Aldi Priya Perdana <aldidana@gmail.com>
2016-06-23 01:48:31 +0700

    Chain middleware for httprouter

---
+++ main.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+//Middlewares type
+type Middlewares func(res http.ResponseWriter, request *http.Request, p httprouter.Params) error
+
+//Chain for Router
+func Chain(m ...Middlewares) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		for _, middleware := range m {
+			err := middleware(w, r, p)
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				return
+			}
+		}
+	}
+}
